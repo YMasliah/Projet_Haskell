35826c5e3b82a5fef36e953d087042ee5c80303d
Yann Masliah <yann.masliah@etu.univ-amu.fr>
2016-11-23 15:55:23 +0100

    plus que lzwDecode a faire, tester ce qui a ete fait

--- Main.hs
+++ Main.hs
@@ -13,11 +13,20 @@ newtype ListeAssociative = L [(Code,String)]
 instance Table ListeAssociative where
 	empty = L [] 
 	ajouter L [] b = L [(0,b)]
-	ajouter L a b = L [a:(fst(last a)+1,b)]
+	ajouter L a b = L (a ++ [(fst(last a)+1,b)])
 	codeOf L a b = lookupbis b a
 	stringOf L a b = lookup b a
 	isIn L a b = if codeOf L a b == Nothing then False else True
-	split L a b = isIn L a b
+	split L a b =	(prefix, prefixCode, suffixe) 
+								where
+									prefix = findIsInMax a b []
+									prefixCode = codeOf a prefix
+									suffixe = drop (length prefix) b
+	
+findisInMax :: Table a => a -> String -> String -> String
+findIsInMax L a (b:bs) c = if isIn L a word == True then findIsInMax L a bs word else c
+	where
+		word = (c ++ [b])
 	
 lookupbis                  :: (Eq b) => b -> [(a,b)] -> Maybe a
 lookupbis _key []          =  Nothing
