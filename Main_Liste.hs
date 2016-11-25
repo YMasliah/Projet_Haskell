616235f4b4ff1e4fe4523016b33ea0a94de77c80
Yann Masliah <yann.masliah@etu.univ-amu.fr>
2016-11-25 08:39:52 +0100

    debut arbre

--- Main_Liste.hs
+++ Main_Liste.hs
@@ -2,8 +2,6 @@ import Test.QuickCheck
 
 type Code = Int
 
-data Apref = Apref [(Code,Char,Apref)] deriving (Show,Eq)
-
 class Table a where
 	empty :: a 
 	ajouter :: a -> String -> a
@@ -11,27 +9,6 @@ class Table a where
 	stringOf :: a -> Code -> Maybe String
 	isIn :: a -> String -> Bool
 	split :: a -> String -> (String,Maybe Code,String)
-
-instance Table Apref where
-	empty = (Apref []) 
-	ajouter (Apref []) [b] = (Apref [(0,b,empty)])
-	ajouter (Apref a) b =  
-{-	codeOf :: a -> String -> Maybe Code
-	stringOf :: a -> Code -> Maybe String
-	isIn :: a -> String -> Bool
-	split :: a -> String -> (String,Maybe Code,String)-}
-
-findIsInMax :: ListeAssociative -> String -> String -> String
-findIsInMax (L a) [] c = c
-findIsInMax (L a) (b:bs) c = if isIn (L a) word == True then findIsInMax (L a) bs word else c
-	where
-		word = (c ++ [b])
-	
-lookupbis                  :: (Eq b) => b -> [(a,b)] -> Maybe a
-lookupbis _key []          =  Nothing
-lookupbis  key ((x,y):xys)
-    | key == y          =  Just x
-    | otherwise         =  lookupbis key xys
 
 newtype ListeAssociative = L [(Code,String)]
 													deriving Show
@@ -48,7 +25,7 @@ instance Table ListeAssociative where
 									prefix = findIsInMax (L a) b []
 									prefixCode = codeOf (L a) prefix
 									suffixe = drop (length prefix) b
-{-	
+	
 findIsInMax :: ListeAssociative -> String -> String -> String
 findIsInMax (L a) [] c = c
 findIsInMax (L a) (b:bs) c = if isIn (L a) word == True then findIsInMax (L a) bs word else c
@@ -60,7 +37,7 @@ lookupbis _key []          =  Nothing
 lookupbis  key ((x,y):xys)
     | key == y          =  Just x
     | otherwise         =  lookupbis key xys	
--}	
+	
 -- regarder le wikipedia  
 -- le encode recupere une chaine de caractere
 -- il regarde caractere par caractere, tant qu'il reconnais le couple de caractere il continue, d'es qu'il en trouve un nouveau le rajoute dans la table et lui met une valeur
@@ -101,19 +78,17 @@ genSafeString = vectorOf 20 genSafeChar
 		
 test :: ListeAssociative -> ListeAssociative 
 test (L a) = ajouter (ajouter (L a) "tata") "toto"
-{-
+
 grosTest :: ListeAssociative -> [Bool]
 grosTest (L a) = replicate 20 (if string == string2 then True else False) 
 	where
 		string = genSafeString
 		string2 = lzwDecode (L a) (lzwEncode (L a) string)
--}
+
 valeur :: ListeAssociative
 valeur = L [(0,"a"),(1,"b"),(2,"c")]
 code :: [Int]
 code = [0,1,3,2,4,7,0,9,10,0]
 string = "ababcbababaaaaaaa"
 
-arbre :: Apref
-arbre = Apref [(0,'a',(Apref [(4,'b',(Apref []))])),(1,'b',(Apref [])),(3,'c',(Apref[]))]
 
