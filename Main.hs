6882a5f363f94de06a7c7ebcbb46833e8644e3eb
YMasliah <yann.masliah@etu.univ-amu.fr>
2016-11-26 00:02:58 +0100

    toujours pas reussi a reparer la variable code de ajouter

--- Main.hs
+++ Main.hs
@@ -2,74 +2,76 @@ import Test.QuickCheck
 
 type Code = Int
 
-data Apref = Apref [(Code,Char,Apref)] deriving (Show,Eq)
+data Apref = Apref [(Char,Code,Apref)] deriving (Show,Eq)
 
 class Table a where
-	empty :: a 
-	ajouter :: a -> String -> a
-	codeOf :: a -> String -> Maybe Code
-	stringOf :: a -> Code -> Maybe String
-	isIn :: a -> String -> Bool
-	split :: a -> String -> (String,Maybe Code,String)
+  empty :: a 
+  ajouter :: a -> String -> a
+  codeOf :: a -> String -> Maybe Code
+  stringOf :: a -> Code -> Maybe String
+  isIn :: a -> String -> Bool
+  split :: a -> String -> (String,Maybe Code,String)
 
 instance Table Apref where
-	empty = (Apref []) 
-	ajouter (Apref a) [b] = (Apref (a ++ [(0,b,empty)]))
-	ajouter (Apref (a:autres)) (d:bs) = if b == d then ajouter c bs else ajouter (Apref autres) (d:bs)
-		where
-			(_,b,c) = a 
+  empty = (Apref []) 
+  ajouter (Apref a) [b] = (Apref (a ++ [(b,0,empty)]))
+  ajouter (Apref (d:autres)) (e:bs) = if a == e then Apref ((a,b,(ajouter c bs)):autres) else ajouter (Apref autres) (e:bs)
+    where
+      (a,b,c) = d 
+	  code = nbnoeud (Apref d)
 --ajouter ECHECCC TRY AGAIN ! xD
-{-	codeOf :: a -> String -> Maybe Code
-	stringOf :: a -> Code -> Maybe String
-	isIn :: a -> String -> Bool
-	split :: a -> String -> (String,Maybe Code,String)
+{-  codeOf :: a -> String -> Maybe Code
+  stringOf :: a -> Code -> Maybe String
+  isIn :: a -> String -> Bool
+  split :: a -> String -> (String,Maybe Code,String)
 
 findIsInMax :: ListeAssociative -> String -> String -> String
 findIsInMax (L a) [] c = c
 findIsInMax (L a) (b:bs) c = if isIn (L a) word == True then findIsInMax (L a) bs word else c
-	where
-		word = (c ++ [b])
-	
+  where
+    word = (c ++ [b])
+  
 lookupbis                  :: (Eq b) => b -> [(a,b)] -> Maybe a
 lookupbis _key []          =  Nothing
 lookupbis  key ((x,y):xys)
     | key == y          =  Just x
     | otherwise         =  lookupbis key xys-}
-
+	
+nbnoeud :: Apref -> Code 
+nbnoeud (Apref []) = 0
+nbnoeud (Apref (d:autres)) = 1 + nbnoeud c + nbnoeud (Apref autres)
+	where
+		  (a,b,c) = d 
+		  
 newtype ListeAssociative = L [(Code,String)]
-								deriving Show
+                deriving Show
 
 instance Table ListeAssociative where
-	empty = (L []) 
-	
-	ajouter (L []) b = (L [(0,b)])
-	ajouter (L a) b = (L (a ++ [(fst(last a)+1,b)]))
-	
-	codeOf (L a) b = lookupbis b a
-	
-	stringOf (L a) b = lookup b a
-	
-	isIn (L a) b = if codeOf (L a) b == Nothing then False else True
-	
-	split (L a) b =	(prefix, prefixCode, suffixe) 
-								where
-									prefix = findIsInMax (L a) b []
-									prefixCode = codeOf (L a) prefix
-									suffixe = drop (length prefix) b
+  empty = (L []) 
+  ajouter (L []) b = (L [(0,b)])
+  ajouter (L a) b = (L (a ++ [(fst(last a)+1,b)]))
+  codeOf (L a) b = lookupbis b a
+  stringOf (L a) b = lookup b a
+  isIn (L a) b = if codeOf (L a) b == Nothing then False else True
+  split (L a) b =  (prefix, prefixCode, suffixe) 
+        where
+          prefix = findIsInMax (L a) b []
+          prefixCode = codeOf (L a) prefix
+          suffixe = drop (length prefix) b
 
 findIsInMax :: ListeAssociative -> String -> String -> String
 findIsInMax (L a) [] c = c
 findIsInMax (L a) (b:bs) c = if isIn (L a) word == True 
-									then findIsInMax (L a) bs word
-									else c
-							where
-								word = (c ++ [b])
-	
+                then findIsInMax (L a) bs word
+                else c
+              where
+                word = (c ++ [b])
+  
 lookupbis                  :: (Eq b) => b -> [(a,b)] -> Maybe a
 lookupbis _key []          =  Nothing
 lookupbis  key ((x,y):xys)
     | key == y          =  Just x
-    | otherwise         =  lookupbis key xys	
+    | otherwise         =  lookupbis key xys  
 
 -- regarder le wikipedia  
 -- le encode recupere une chaine de caractere
@@ -77,46 +79,48 @@ lookupbis  key ((x,y):xys)
 --
 
 lzwEncode :: Table a => a -> String -> [Code]
-lzwEncode table bdd = if suffixe == [] 
-											then [prefixCode] --ajouter (L table) prefix
-											else [prefixCode] ++ lzwEncode (ajouter table (prefix ++ [(head suffixe)])) suffixe 
-				where
-					(prefix,Just prefixCode,suffixe) = split table bdd 
-					
+lzwEncode table bdd = 
+  if suffixe == [] 
+  then [prefixCode] --ajouter (L table) prefix
+  else [prefixCode] ++ lzwEncode (ajouter table (prefix ++ [(head suffixe)])) suffixe 
+  where
+    (prefix,Just prefixCode,suffixe) = split table bdd 
+          
 fromJust          :: Maybe a -> a
 fromJust (Just x) = x
 
 lzwDecode :: Table a => a  -> [Code] -> String
 lzwDecode table (code:codes) = word ++ lzw_Decode table word codes
-	where
-		Just word = stringOf table code
-		
+  where
+    Just word = stringOf table code
+    
 lzw_Decode :: Table a => a  -> String -> [Code] -> String
 lzw_Decode table prefix [] = [] --ajouter (L table) prefix
-lzw_Decode table prefix (code:codes) = if word == Nothing 
-																						then newWord ++ lzw_Decode newTable newWord codes
-																						else fromJust word ++ lzw_Decode newTable (fromJust word) codes
-	where
-		word = stringOf table code
-		newWord = (prefix ++ [(head prefix)])
-		newTable = ajouter table newWord
+lzw_Decode table prefix (code:codes) = 
+  if word == Nothing 
+  then newWord ++ lzw_Decode newTable newWord codes
+  else fromJust word ++ lzw_Decode newTable (fromJust word) codes
+  where
+    word = stringOf table code
+    newWord = (prefix ++ [(head prefix)])
+    newTable = ajouter table newWord
 
 -- la zone de test 
 
 genSafeChar :: Gen Char
 genSafeChar = elements ['a'..'c']
-		
+    
 genSafeString :: Gen String
-genSafeString = vectorOf 20 genSafeChar		
-		
+genSafeString = vectorOf 20 genSafeChar    
+    
 test :: ListeAssociative -> ListeAssociative 
 test (L a) = ajouter (ajouter (L a) "tata") "toto"
 {-
 grosTest :: ListeAssociative -> [Bool]
 grosTest (L a) = replicate 20 (if string == string2 then True else False) 
-	where
-		string = genSafeString
-		string2 = lzwDecode (L a) (lzwEncode (L a) string)
+  where
+    string = genSafeString
+    string2 = lzwDecode (L a) (lzwEncode (L a) string)
 -}
 valeur :: ListeAssociative
 valeur = L [(0,"a"),(1,"b"),(2,"c")]
@@ -126,6 +130,17 @@ code = [0,1,3,2,4,7,0,9,10,0]
 
 string = "ababcbababaaaaaaa"
 
-arbre :: Apref
-arbre = Apref [(0,'a',(Apref [(4,'b',(Apref []))])),(1,'b',(Apref [])),(3,'c',(Apref[]))]
+stringbis = "ab"
 
+arbre :: Apref
+arbre = Apref [('a',0,(Apref [('b',3,(Apref []))])),('b',1,(Apref [])),('c',2,(Apref[]))]
+
+arbrebis :: Apref
+arbrebis = Apref [('a',0,(Apref [])),('b',1,(Apref [])),('c',2,(Apref[]))]
+
+
+
+
+
+
+ 
